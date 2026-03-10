bf4c8145b348afd55579e44a20d3a1438d1c1987
mateoworld <mateo@homies.love>
2026-03-09 18:57:46 -0600

    modules: update theme.nix with matugen and KDE color config

--- modules/theme.nix
+++ modules/theme.nix
@@ -2,6 +2,10 @@
 let
   enabled = config.illogical-impulse.enable;
   cursor = config.illogical-impulse.theme.cursor;
+  selfPkgs = import ../pkgs {
+    inherit pkgs;
+    quickshell = config.illogical-impulse.hyprland.quickshellPackage;
+  };
 in
 {
   config = lib.mkIf enabled {
@@ -10,19 +14,27 @@ in
 
     # Cursor
     home.sessionVariables = {
-      XCURSOR_THEME = cursor.theme;
+      XCURSOR_THEME = cursor.name;
       XCURSOR_SIZE = 24;
     };
 
     home.pointerCursor = {
       package = cursor.package;
-      name = cursor.theme;
+      name = cursor.name;
       size = 24;
       gtk.enable = true;
     };
 
-    home.file.".local/share/icons/MoreWaita" = {
-      source = "${pkgs.morewaita-icon-theme}/share/icons";
+    # Matugen config (replaces gradience/pywal for color generation)
+    home.file.".config/matugen" = {
+      source = "${selfPkgs.illogical-impulse-dotfiles}/matugen";
+      recursive = true;
+    };
+
+    # KDE Material You Colors config
+    home.file.".config/kde-material-you-colors" = {
+      source = "${selfPkgs.illogical-impulse-dotfiles}/kde-material-you-colors";
+      recursive = true;
     };
   };
 }
